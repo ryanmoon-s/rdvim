b90bdc0dc6e9a81a5093c53c68f721afb77025c7
arashi <arashi@tencent.com>
2022-01-16 16:14:18 +0800

    fix coc.sh; close coc.nvim plug; add ycm dir

--- coc.sh
+++ coc.sh
@@ -1,15 +1,21 @@
 #########################################################################
-# File Name:    coc_dependence_install.sh
+# File Name:    coc.sh
 # Author:       arashi
 # mail:         arashi@tencent.com
 # Created Time: 2021年12月31日 星期五 17时43分15秒
 #########################################################################
 #!/bin/bash
 
-# 此脚本为linux专用 
-# mac请执行：brew install nodejs; brew install llvm
+# 此脚本安装nodejs clangd
+# mac    不用运行脚本   brew安装即可：brew install nodejs; brew install llvm
+# linux  如果商店能安装 也不用运行脚本 
 
-# nodejs and clangd(c++)
+# 若为 linux 且商店没有上面两种 
+# 运行 sh / zsh coc.sh
+
+# 解释
+# coc.nvim依赖nodejs 
+# clangd 为 c++ 的语言协议服务器
 
 # nodejs
 coc=coc_dependence
